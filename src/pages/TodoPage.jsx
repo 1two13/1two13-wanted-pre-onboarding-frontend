48b84921ff2598b4b2f448700ef7836fb2312271
Tory <88531407+1two13@users.noreply.github.com>
2023-04-07 11:14:15 +0900

    feat: 투두 리스트 목록 보여주기

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,5 +1,20 @@
 function TodoPage() {
-  return;
+  return (
+    <div className="py-[4%] px-[5%]">
+      <li>
+        <label>
+          <input type="checkbox" className="mr-[10px]" />
+          <span>TODO 1</span>
+        </label>
+      </li>
+      <li>
+        <label>
+          <input type="checkbox" className="mr-[10px]" />
+          <span>TODO 2</span>
+        </label>
+      </li>
+    </div>
+  );
 }
 
 export default TodoPage;
