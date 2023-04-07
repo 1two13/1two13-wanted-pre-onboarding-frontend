5047cfaa991fca6bcbfef8b66ca3c572caef95e8
Tory <88531407+1two13@users.noreply.github.com>
2023-04-07 21:13:43 +0900

    feat: 새로운 TODO를 입력할 수 있는 input, 추가 button 생성

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,18 +1,18 @@
+import TodoForm from '../components/common/TodoForm';
+
 function TodoPage() {
   return (
-    <div className="py-[4%] px-[5%]">
-      <li>
-        <label>
-          <input type="checkbox" className="mr-[10px]" />
-          <span>TODO 1</span>
-        </label>
-      </li>
-      <li>
-        <label>
-          <input type="checkbox" className="mr-[10px]" />
-          <span>TODO 2</span>
-        </label>
-      </li>
+    <div className="py-[3%] px-[5%]">
+      <h1 className="mb-[3%] font-bold text-5xl">Todo</h1>
+      <div className="mb-[1%]">
+        <input data-testid="new-todo-input" className="px-[5px] mr-[10px] border-[1px]" />
+        <button data-testid="new-todo-add-button" className="px-[2px] border-[1px] bg-lightGray">
+          추가
+        </button>
+      </div>
+      <div>
+        <TodoForm title={'TODO 1'} />
+      </div>
     </div>
   );
 }
