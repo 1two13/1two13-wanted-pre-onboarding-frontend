fba52b1fefb77c8e2d4406b33d16b366465ca627
Tory <88531407+1two13@users.noreply.github.com>
2023-04-11 15:44:22 +0900

    feat: 수정, 삭제 버튼 생성

--- src/components/common/TodoForm.jsx
+++ src/components/common/TodoForm.jsx
@@ -1,10 +1,16 @@
 function TodoForm({ el, onChange, checked }) {
   return (
     <li>
-      <label>
+      <label className="mr-[10px]">
         <input type="checkbox" onChange={onChange} checked={checked} className="mr-[10px]" />
         <span>{el.todo}</span>
       </label>
+      <button data-testid="modify-button" className="m-[1px] border-[1px] bg-lightGray">
+        수정
+      </button>
+      <button data-testid="delete-button" className="m-[1px] border-[1px] bg-lightGray">
+        삭제
+      </button>
     </li>
   );
 }
