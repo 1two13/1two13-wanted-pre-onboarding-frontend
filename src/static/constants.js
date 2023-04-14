2417af2acd5c1026d142f75dd5be7ac8b8cfd928
Tory <88531407+1two13@users.noreply.github.com>
2023-04-14 13:13:19 +0900

    feat: 상수 코드 따로 관리

---
+++ src/static/constants.js
@@ -0,0 +1,33 @@
+const URL = 'https://www.pre-onboarding-selection-task.shop';
+const EMAIL_REQUIREMENT = '이메일에는 @가 포함되어야 합니다.';
+const PASSWORD_REQUIREMENT = '비밀번호는 8자 이상이어야 합니다.';
+const AVAILABLE_EMAIL = '사용가능한 이메일입니다.';
+const AVAILABLE_PASSWORD = '사용가능한 비밀번호입니다.';
+const CREATE_TODO = '할 일을 등록해 주세요.';
+const SIGN_UP = '회원가입';
+const SIGN_IN = '로그인';
+const EMAIL = '이메일';
+const PASSWORD = '비밀번호';
+const ADD = '추가';
+const SUBMIT = '제출';
+const CANCEL = '취소';
+const MODIFY = '수정';
+const DELETE = '삭제';
+
+export {
+  URL,
+  EMAIL_REQUIREMENT,
+  PASSWORD_REQUIREMENT,
+  AVAILABLE_EMAIL,
+  AVAILABLE_PASSWORD,
+  CREATE_TODO,
+  SIGN_UP,
+  SIGN_IN,
+  EMAIL,
+  PASSWORD,
+  ADD,
+  SUBMIT,
+  CANCEL,
+  MODIFY,
+  DELETE,
+};
